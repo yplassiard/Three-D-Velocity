65b6c07b33bba1490024306b1d86bef31df8094c
Yannick Plassiard <plassiardyannick@gmail.com>
2020-02-17 10:43:47 +0100

    [translate] Translated updater

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -9,18 +9,18 @@ namespace Updater
 		static void Main(string[] args)
 		{
 			String runAfter = args[args.Length - 1];
-			System.Console.WriteLine("Extracting downloaded zip file...");
+			System.Console.WriteLine("Décompression de la mise à jour...");
 			if (Directory.Exists("update"))
 				Directory.Delete("update", true);
 			ZipFile.ExtractToDirectory("Three-D-Velocity-Binaries-master.zip", "update");
-			System.Console.WriteLine("Copying new files...");
+			System.Console.WriteLine("Copie des nouveaux fichiers...");
 			DirectoryCopy("update\\Three-D-Velocity-Binaries-master", ".", true);
-			System.Console.WriteLine("Cleaning up temporary files...");
+			System.Console.WriteLine("Nettoyage des fichiers temporaires...");
 			Directory.Delete("update", true);
 			File.Delete("Three-D-Velocity-Binaries-master.zip");
-			System.Console.WriteLine("Done! Starting " + runAfter + "...");
+			System.Console.WriteLine("Terminé! Démarrage de " + runAfter + "...");
 			System.Diagnostics.Process.Start(runAfter);
-			System.Console.WriteLine("Exiting setup");
+			System.Console.WriteLine("Sortie");
 		}
 
 		private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
@@ -30,7 +30,7 @@ namespace Updater
 
 			if (!dir.Exists) {
 				throw new DirectoryNotFoundException(
-					"Source directory does not exist or could not be found: "
+					"Le dossier source n'existe pas: "
 					+ sourceDirName);
 			}
 
